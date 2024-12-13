b0e1d89cc20e07c4b05338da84bf469e1ede0ef0
ralphbetta <ralphbetta@gmail.com>
2024-12-13 15:22:33 +0100

    complete the escrow smart contract system

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -82,13 +82,6 @@ contract Escrow {
         approval[_nftID][msg.sender] = true;
     }
 
-    // -> this will allwow the smart contract receive ether - to accept ether to the contract.
-    receive() external payable {}
-
-    function getBalance() public view returns (uint256) {
-        return address(this).balance; //ether balance. balance of smart contract
-    }
-
     // Finalize Sale
     // -> Require inspection status (add more items here, like appraisal)
     // -> Require sale to be authorized
@@ -105,9 +98,28 @@ contract Escrow {
         isListed[_nftID] = false;
 
         //-> transfer ether from contract to seller
-        (bool success, ) = payable(seller).call{value: address(this).balance}("");
+        (bool success, ) = payable(seller).call{value: address(this).balance}(
+            ""
+        );
         require(success);
 
         IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
+    }
+
+    // Cancel Sale (handle earnest deposit)
+    // -> if inspection status is not approved, then refund, otherwise send to seller
+    function cancelSale(uint256 _nftID) public {
+        if (inspectionPassed[_nftID] == false) {
+            payable(buyer[_nftID]).transfer(address(this).balance);
+        } else {
+            payable(seller).transfer(address(this).balance);
+        }
+    }
+
+    // -> this will allwow the smart contract receive ether - to accept ether to the contract.
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance; //ether balance. balance of smart contract
     }
 }
